61e4571bb6bb806fc7a94640180966313d13b888
Mxx1029 <lia.pim@icloud.com>
2021-05-18 13:43:16 +0200

    Initial import

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 class Employee {
 	constructor (firstname, lastname) {
-		this.firstname = firstname
-		this.lastname = lastname
-		// Complete the code!
-	}
-}
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.fullname = `${firstname} ${lastname}`;	
+		this.email = `${firstname}.${lastname}@company.com`.toLowerCase();	
+	};
+};
+
+emp1 = new Employee("John", "Smith")
+console.log(emp1.fullname) // "John Smith"
+
+emp2 = new Employee("Mary",  "Sue")
+console.log(emp2.email) // "mary.sue@company.com"
+
+emp3 = new Employee("Antony", "Walker")
+console.log(emp3.firstname) // "Antony"
